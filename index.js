ca9c3f78bb619fc8077f1f41366997068bbd9d2f
Michael Koscinski <michael.koscinski@gmail.com>
2020-09-01 16:23:41 -0400

    Add players to be added later

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-autographGallery = document.querySelector('.autographs');
+// TODO: Will need to revise this (and the class names) once I add sports cards to the site as well
+
+autographGallery = document.querySelector('.gallery');
 
 function formatNameForFile(name) {
 	return formattedName = name.toLowerCase().replace(/ /g, '-');
@@ -11,25 +13,22 @@ function Player(name, team, position) {
 	this.file = `${formatNameForFile(this.name)}-autograph.jpg`;
 }
 
-// First, generate Player object for each individual autograph
-// Store each Player object in an array (avail for forEach)
-
+// Generate Player objects; store in Array (use forEach later)
 const players = [
 	new Player('Adam Jones', 'Baltimore Orioles', 'Outfielder'),
 	new Player('Andy Pettitte', 'New York Yankees', 'Pitcher'),
 	new Player('David Ortiz', 'Boston Red Sox', 'Designated Hitter'),
-	new Player('Derek Jeter', 'New York Yankees', 'Shortstop'),
+	new Player('Derek Jeter', 'New York Yankees', 'Infielder'),
 	new Player('Hank Aaron', 'Atlanta Braves', 'Outfielder'),
 	new Player('Jorge Posada', 'New York Yankees', 'Catcher'),
 	new Player('Yogi Berra', 'New York Yankees', 'Catcher'),
 ];
 
-// Then, generate HTML cards for each player
-
+// Generate HTML cards for each player
 function makePlayerCardHTML (player) {
 	return cardHTML = `
-		<div class="card autograph">
-			<img src="${player.file}" alt="${player.name} autographed baseball" title="${player.name}">
+		<div class="card">
+			<img src="./images/${player.file}" alt="${player.name} autographed baseball" title="${player.name}">
 			<div class ="caption autograph">
 				<p>${player.name}</p>
 				<p>${player.team}</p>
@@ -39,10 +38,9 @@ function makePlayerCardHTML (player) {
 	`;
 }
 
-
 players.forEach(el => {
 	const playerCardHTML = makePlayerCardHTML(el);
-
+	autographGallery.insertAdjacentHTML('beforeend', playerCardHTML);
 });
 
 
@@ -54,3 +52,51 @@ players.forEach(el => {
 
 
 // When user clicks into modal, generate new HTML? Or should I pre-generate? (Each player will have different... Could have arrays for each player's accolades, then accolades.forEach generate a <ul> to be added to the description card within the <p>accolades</p>)
+
+// TODO: ignore this until i have files for each photo
+const playersToAddLater = [
+	new Player('Akinori Iwamura',	'Tampa Bay Rays', 'Infielder'),
+	new Player('Alex Gordon',	'Kansas City Royals', 'Outfielder'),
+	new Player('Alex Rodriguez', 'Yankees', 'Infielder'),
+	new Player('Aubrey Huff',	'Baltimore Orioles', 'Infielder'),
+	new Player('Billy Ripken',	'Baltimore Orioles', 'Infielder'),
+	new Player('Boof Bonsor',	'Minnesota Twins', 'Pitcher'),
+	new Player('Brian Bannister',	'Kansas City Royals', 'Pitcher'),
+	new Player('Brian Bruney',	'New York Yankees', 'Pitcher'),
+	new Player('Brian Burres',	'Baltimore Orioles', 'Pitcher'),
+	new Player('Brian Matusz',	'Baltimore Orioles', 'Pitcher'),
+	new Player('Brian Roberts',	'Baltimore Orioles', 'Infielder'),
+	new Player('Denard Span', 'Minnesota Twins', 'Outfielder'),
+	new Player('Dennis Sarfate', 'Fukuoka SoftBank Hawks', 'Pitcher'),
+	new Player('Dick Hall',	'Baltimore Orioles', 'Pitcher'),
+	new Player('Edwar Ramirez',	'New York Yankees', 'Pitcher'),
+	new Player('Edwin Jackson',	'Tampa Bay Rays', 'Pitcher'),
+	new Player('Erik Beddard',	'Baltimore Orioles', 'Pitcher'),
+	new Player('Garrett Olson', 'Baltimore Orioles', 'Pitcher'),
+	new Player('George Sherrill',	'Baltimore Orioles', 'Pitcher'),
+	new Player('James Shields',	'Tampa Bay Rays', 'Pitcher'),
+	new Player('Jamie Walker',	'Baltimore Orioles', 'Pitcher'),
+	new Player('Jered Weaver',	'Los Angeles Angels', 'Pitcher'),
+	new Player('Jim Hickey',	'Tampa Bay Rays', 'Pitching Coach'),
+	new Player('Jim Palmer', 'Baltimore Orioles', 'Pitcher'),
+	new Player('Joe Maddon',	'Tampa Bay Rays', 'Manager'),
+	new Player('Justin Masterson', 'Boston Red Sox', 'Pitcher'), 
+	new Player('Kevin Millar', 'Baltimore Orioles', 'Infielder'),
+	new Player('Kevin Slowey',	'Minnesota Twins', 'Pitcher'),
+	new Player('Larry Sheets',	'Baltimore Orioles'),
+	new Player('Luke Scott',	'Baltimore Orioles', 'Outfielder'),
+	new Player('Matt Garza', 'Tampa Bay Rays', 'Pitcher'),
+	new Player('Mark Grudzielanek',	'Kansas City Royals', 'Infielder'),
+	new Player('Mark Teahen',	'Kansas City Royals', 'Outfielder'),
+	new Player('Melvin Mora',	'Baltimore Orioles', 'Infielder'),
+	new Player('Nelson Figueroa',	'New York Mets', 'Pitcher'),
+	new Player('Nick Swisher', 'New York Yankees', 'Outfielder'),
+	new Player('Paul Blair',	'Baltimore Orioles', 'Outfielder'),
+	new Player('Rick Dempsey',	'Baltimore Orioles', 'Catcher'),
+	new Player('Scott Kazmir',	'Tampa Bay Rays', 'Pitcher'),
+	new Player('Sean Casey',	'Boston Red Sox', 'Infielder'),
+	new Player('Terry Francona', 'Boston Red Sox', 'Manager'),
+	new Player('Tim Kurkjian', 'ESPN', 'Reporter'),
+	new Player('Tim Teufel', 'New York Mets', 'Infielder'),
+	new Player('Torii Hunter', 'Los Angeles Angels', 'Outfielder'),
+];
